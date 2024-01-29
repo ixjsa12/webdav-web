93472d7e518a5c001e397fd0f112be007145aa01
CodeSandbox <support@codesandbox.io>
2024-01-29 07:42:33 +0000

    Initial commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,35 +1,98 @@
-import { useState } from "react";
-import reactLogo from "./assets/react.svg";
-import viteLogo from "/vite.svg";
-import "./App.css";
-
+import AppBar from "@mui/material/AppBar";
+import FolderIcon from "@mui/icons-material/Folder";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Container from "@mui/material/Container";
+import CssBaseline from "@mui/material/CssBaseline";
+import Breadcrumbs from "@mui/material/Breadcrumbs";
+import Link from "@mui/material/Link";
+import List from "@mui/material/List";
+import ListItem from "@mui/material/ListItem";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import { useLocation, useNavigate } from "react-router-dom";
+import { useState, useEffect } from "react";
 function App() {
-  const [count, setCount] = useState(0);
-
+  const { pathname } = useLocation();
+  const navigate = useNavigate();
+  const [files, setFiles] = useState([]);
+  const [pass, setPass] = useState(false);
+  const path_arr: string[] = pathname.replace(/^\/|\/$/g, "").split("/");
+  const routes = path_arr.map((e) => {
+    let result = "";
+    const index = path_arr.findIndex((k) => k === e);
+    for (let i = 0; i <= index; i++) {
+      result += `/${path_arr[i]}`;
+    }
+    return (
+      <Link underline="hover" color="inherit" href={result}>
+        {decodeURI(e)}
+      </Link>
+    );
+  });
+  console.log("files", files);
+  useEffect(() => {
+    setPass(false);
+    fetch(`https://z4tykw-8787.csb.app/webdav/query${pathname}`)
+      .then((rs) => rs.json())
+      .then((rs) => {
+        if (rs.status == 200) {
+          setFiles(rs.data);
+        } else if (rs.status == 401) {
+          setPass(true);
+        }
+      });
+  }, [pathname]);
   return (
     <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
+      <Box sx={{ display: "flex" }}>
+        <CssBaseline />
+        <AppBar component="nav" position="static">
+          <Toolbar>
+            <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
+              MUI
+            </Typography>
+            <Box sx={{ display: { xs: "none", sm: "block" } }}></Box>
+          </Toolbar>
+        </AppBar>
+      </Box>
+      <Box component="main" sx={{ p: 3 }}>
+        <Container fixed maxWidth="sm">
+          <Breadcrumbs aria-label="breadcrumb">
+            <Link underline="hover" color="inherit" href="/">
+              根路径
+            </Link>
+            {routes}
+          </Breadcrumbs>
+          {pass ? (
+            <h1>需要密码哦</h1>
+          ) : (
+            <List>
+              {files.map((e: any) => (
+                <ListItem
+                  onClick={() => {
+                    if (e.size > 0) {
+                      window.open("/webdav/down" + pathname + e.path);
+                    } else {
+                      navigate(pathname + e.path);
+                    }
+                  }}
+                >
+                  <ListItemIcon>
+                    <FolderIcon />
+                  </ListItemIcon>
+                  <ListItemText
+                    primary={decodeURI(e.path).replace("/", "")}
+                    // secondary="Secondary text"
+                  />
+                </ListItem>
+              ))}
+            </List>
+          )}
+        </Container>
+      </Box>
     </>
   );
 }
-
 export default App;
--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,58 @@
-import React from "react";
 import ReactDOM from "react-dom/client";
+import React from "react";
 import App from "./App.tsx";
 import "./index.css";
+import { ThemeProvider, createTheme } from "@mui/material/styles";
+import { LinkProps } from "@mui/material/Link";
+import {
+  HashRouter,
+  Routes,
+  Route,
+  Link as RouterLink,
+  LinkProps as RouterLinkProps,
+} from "react-router-dom";
+function video() {
+  return (
+    <>
+      <h1>xxx</h1>
+    </>
+  );
+}
+const LinkBehavior = React.forwardRef<
+  HTMLAnchorElement,
+  Omit<RouterLinkProps, "to"> & { href: RouterLinkProps["to"] }
+>((props, ref) => {
+  const { href, ...other } = props;
+  // Map href (MUI) -> to (react-router)
+  return (
+    <RouterLink data-testid="custom-link" ref={ref} to={href} {...other} />
+  );
+});
 
+const theme = createTheme({
+  components: {
+    MuiLink: {
+      defaultProps: {
+        component: LinkBehavior,
+      } as LinkProps,
+    },
+    MuiButtonBase: {
+      defaultProps: {
+        LinkComponent: LinkBehavior,
+      },
+    },
+  },
+});
 ReactDOM.createRoot(document.getElementById("root")!).render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <ThemeProvider theme={theme}>
+    <HashRouter basename="/">
+      <Routes>
+        <Route path="/video/:path*" Component={video} />
+        <Route path="/pdf/:path*" Component={video} />
+        <Route path="/png/:path*" Component={video} />
+        <Route path="/" Component={App} />
+        <Route path="/:path*" Component={App} /> {/* 👈 Renders at /#/app/ */}
+      </Routes>
+    </HashRouter>
+  </ThemeProvider>,
 );
